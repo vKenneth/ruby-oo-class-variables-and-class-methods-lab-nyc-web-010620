f1763b1be05ae90f8382845b46a4f1c62e30f9a1
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-09 04:39:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,9 @@ thriller = Song.new("Thriller", "Michael Jackson", "pop")
 gods_plan = Song.new("God's Plan", "Drake", "rap")
 
 
-def initialize()
+def initialize(count)
+  @@count << 0 
+  
 
 
 

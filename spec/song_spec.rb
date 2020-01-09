3859c4970c410d9a724e499f290ce1c25adf71a3
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-09 04:47:29 +0000

    Done.

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -82,3 +82,24 @@ describe "Song" do
     end
   end
 end
+
+
+binding.pry 
+
+class Song 
+  attr_accessor :name, :artist, :genre 
+  def initialize(name)
+    @name = name
+  end
+  def initialize(artist)
+    @artist = artist
+  end
+  def initialize(genre)
+    @genre = genre
+  end
+end
+
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z" "rap")
+thriller = Song.new("Thriller", "Michael Jackson", "pop")
+gods_plan = Song.new("God's Plan", "Drake", "rap")
+
